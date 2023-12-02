ed329020d8e97295300d1f1c94e953391ee8c105
AGirimaj <Amit.Girimaji@Analog.com>
2023-12-02 11:26:47 +0530

    Day 2 Part 2 Cube Conundrum

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -8,10 +8,9 @@ namespace AdventOfCode
 {
     class Program
     {
-        //Day 2: Cubes
-        //12 red cubes, 13 green cubes, and 14 blue cubes.
-        //2449 ans
-       
+        //Day 2- 2: Cubes 2
+        //63981 ans
+
         private static Dictionary<string, int> dict = new()
         {
             { "red", 12 },
@@ -30,13 +29,13 @@ namespace AdventOfCode
             List<long> list = new List<long>();
             foreach (var code in codes)
             {
-                long gameNum = _getValidGameValOr0(code);
+                long gameNum = _getNum(code);
                 list.Add(gameNum);
             }
             return list;
         }
 
-        private static long _getValidGameValOr0(string code)
+        private static long _getNum(string code)
         {
             string[] pieces = code.Split(':');
             Regex regex = new Regex(@"Game (\d+)");
@@ -49,67 +48,41 @@ namespace AdventOfCode
             string gameId = match.Groups[1].Value;
 
             string[] reveals = pieces[1].Split(';');
-            bool isValid = _checkValidity(reveals);
+            Dictionary<string, int> minvaluesRGB = _getMinRGB(reveals);
             Console.WriteLine(code);
-            Console.WriteLine(isValid);
-            if (isValid)
-            {
-                return Convert.ToInt64(gameId);
-            }
-            else
-            {
-                return 0;
-            }
+            Console.WriteLine($"{ minvaluesRGB["red"]} , { minvaluesRGB["green"]} , { minvaluesRGB["blue"]}");
+            long multiply = minvaluesRGB["red"] * minvaluesRGB["green"] * minvaluesRGB["blue"];
+            Console.WriteLine(multiply) ;
+            return multiply;
 
         }
 
-        private static bool _checkValidity(string[] reveals)
+        private static Dictionary<string, int> _getMinRGB(string[] reveals)
         {
+            Dictionary<string, int> maxDict = new()
+            {
+                { "red", 0 },
+                { "green", 0 },
+                { "blue", 0 }
+            };
             foreach (var reveal in reveals)
             {
                 string[] colorCounts = reveal.Split(',');
                 foreach (var colorCount in colorCounts)
                 {
                     string[] number_color = colorCount.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                    if(dict[number_color[1]] < Convert.ToInt32( number_color[0]))
+                    int count = Convert.ToInt32(number_color[0]);
+                    string color = number_color[1];
+                    if (maxDict[color] < count)
                     {
-                        return false;
+                        maxDict[color] = count;
                     }
                 }
 
             }
-            return true;
+            return maxDict;
         }
 
-        private static int _getNum(string code)
-        {
-            List<string> numbers = new List<string>() { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-            List<KeyValuePair<string, int>> matches = new List<KeyValuePair<string, int>>();
-
-            foreach (var str in numbers)
-            {
-                if (code.Contains(str))
-                {
-                    var index = code.IndexOf(str);
-                    var indexLast = code.LastIndexOf(str);
-                    matches.Add(new KeyValuePair<string, int>(str, index));
-                    if (index != indexLast)
-                    {
-                        matches.Add(new KeyValuePair<string, int>(str, indexLast));
-
-                    }
-                }
-            }
-
-            int min = matches.Min(x => x.Value);
-            int max = matches.Max(x => x.Value);
-
-            int first = numbers.IndexOf(matches.First(x => x.Value == min).Key) % 10;
-            int last = numbers.IndexOf(matches.First(x => x.Value == max).Key) % 10;
-
-            return first * 10 + last;
-
-        }
     }
 }
